7a02defd920ca71134beb591c831b197f0354e40
Joseph Putz <josephputz89@gmail.com>
2023-05-11 14:32:16 -0500

    test button

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Counter from "../Counter";
 
 describe(Counter, () => {
@@ -10,5 +10,15 @@ describe(Counter, () => {
         // Don't overuse, only add data-testid when there's no other easy way to identify the component
         expect(countValue).toEqual(0);
         //Can use toBe, toEqual, toHaveLength, etc
+    });
+    it("count should increment by 1 if the increment button is clicked", () => {
+        const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
+        const incrementBttn = getByRole("button", { name: "Increment" })
+        //This gets an element with the role 'button' that has the name Increment
+        const countValue1 = Number(getByTestId("count").textContent)
+        expect(countValue1).toEqual(0)
+        fireEvent.click(incrementBttn)
+        const countValue2 = Number(getByTestId("count").textContent)
+        expect(countValue2).toEqual(1)
     })
 })
